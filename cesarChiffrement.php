94c7eb7afcf660a1d2564fdc3181464abe385e88
metus01 <carnelmetus@gmail.com>
2023-11-24 05:49:04 +0100

    Refactoring of code

--- cesarChiffrement.php
+++ cesarChiffrement.php
@@ -33,42 +33,7 @@ function chiffrementCesar($texte, $decalage)
 
     return $resultat;
 }
-// dechiffrement
-
 function dechiffrementCesar($texte, $decalage)
-{
-    $resultat = "";
-
-    // Parcourir chaque caractère dans le texte
-    $longueur = strlen($texte);
-    for ($i = 0; $i < $longueur; $i++) {
-        $caractere = $texte[$i];
-
-        // Vérifier si le caractère est une lettre
-        if (ctype_alpha($caractere)) {
-            // Déterminer si le caractère est en majuscule ou en minuscule
-            $majuscule = ctype_upper($caractere);
-            $caractere = strtoupper($caractere);
-
-            // Appliquer le déchiffrement de César
-            $caractereDecale = chr((ord($caractere) - 65 - $decalage + 26) % 26 + 65);
-
-            // Reconvertir en minuscule si nécessaire
-            if (!$majuscule) {
-                $caractereDecale = strtolower($caractereDecale);
-            }
-
-            // Ajouter le caractère déchiffré au résultat
-            $resultat .= $caractereDecale;
-        } else {
-            // Si le caractère n'est pas une lettre, ajouter tel quel au résultat
-            $resultat .= $caractere;
-        }
-    }
-
-    return $resultat;
-}
-function simpleDechiffrement($texte, $decalage)
 {
     // dechiffrement  = sens inverse du chiffrement
     // example : chiffrement avec A+3, dechiiffrement donne A+3 - 3
